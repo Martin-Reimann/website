6e93cc86100d9691e107839cba81fbd1dbe1f481
Louis Cutner <lcutner@me.com>
2021-07-05 23:43:40 +0100

    test p2

--- src/index.js
+++ src/index.js
@@ -40,12 +40,14 @@ menu.addEventListener("click", (e) => {
   }
 });
 
-for (image of images) {
+for (let i = 0; i < images.length; i++) {
+  console.log("hi");
+  const image = images[i];
+  console.log(image);
+  const icon = image.children[3];
+  const links = image.nextElementSibling;
   image.addEventListener("click", (e) => {
     console.log("click");
-    const image = e.currentTarget;
-    const icon = image.children[3];
-    const links = image.nextElementSibling;
     if (icon.classList.contains("fa-chevron-circle-down")) {
       for (link of all_links) {
         if (link.classList.contains("flex")) {
@@ -76,3 +78,40 @@ for (image of images) {
     }
   });
 }
+
+// for (image of images) {
+//   image.addEventListener("click", (e) => {
+//     console.log("click");
+//     const image = e.currentTarget;
+//     const icon = image.children[3];
+//     const links = image.nextElementSibling;
+//     if (icon.classList.contains("fa-chevron-circle-down")) {
+//       for (link of all_links) {
+//         if (link.classList.contains("flex")) {
+//           link.classList.remove("flex");
+//           link.classList.add("hidden");
+//         }
+//       }
+//       const up_arrows = document.getElementsByClassName("fa-chevron-circle-up");
+//       for (arrow of up_arrows) {
+//         arrow.classList.remove("fa-chevron-circle-up");
+//         arrow.classList.add("fa-chevron-circle-down");
+//       }
+//       icon.classList.remove("fa-chevron-circle-down");
+//       icon.classList.add("fa-chevron-circle-up");
+//       links.classList.remove("hidden");
+//       links.classList.add("flex");
+//     } else {
+//       icon.classList.add("fa-chevron-circle-down");
+//       icon.classList.remove("fa-chevron-circle-up");
+//       links.classList.add("hidden");
+//       links.classList.remove("flex");
+//       for (link of all_links) {
+//         if (link.classList.contains("flex")) {
+//           link.classList.remove("flex");
+//           link.classList.add("hidden");
+//         }
+//       }
+//     }
+//   });
+// }
