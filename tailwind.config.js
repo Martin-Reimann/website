4adb41f287927aaebf0bf7945582dd3244531dea
Louis Cutner <lcutner@me.com>
2021-07-05 21:10:22 +0100

    updated files

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  darkMode: false,
+  theme: {
+    extend: {
+      colors: {
+        white: "#FFFFFF",
+        "shark-grey": "#1D243B",
+        "bumble-bee-yellow": "#F4D03F",
+      },
+      transitionProperty: {
+        height: "max-height",
+      },
+      spacing: {
+        144: "36 rem",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
